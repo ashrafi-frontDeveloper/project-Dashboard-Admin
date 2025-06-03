145bb3ee4eea5231c4768cc71871c053d6aa9fac
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-06-03 19:35:25 +0330

    style(Home): change style Charts for Home page

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -9,13 +9,15 @@ import {
 } from 'recharts'
 
 export default function Chart({title, data, dataKey, grid}) {
+    console.log(data);
+    
   return (
       <div className="chart">
           <h3 className='chartTitle'>{title}</h3>
           <ResponsiveContainer width="100%" aspect={4}>
               <LineChart data={data}>
                   <XAxis dataKey="name" stroke='#5550bd' />
-                  <Line dataKey={dataKey} stroke='#5550bd' />
+                  <Line type="monotone" dataKey={dataKey} stroke='#5550bd' />
                   <Tooltip />
                   {grid && <CartesianGrid stroke='#e0dfdf' strokeDasharray="10" />}
               </LineChart>
