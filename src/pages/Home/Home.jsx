e07294b9767a8ce334c2856129228834569dd960
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-06-04 21:21:12 +0330

    style(WidgetSm,Home): add section new join members

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { xAxisData } from '../../data';
 import Feature from './../../components/features/Feature'
 import Chart from './../../components/Charts/Charts'
+import WidgetSm from '../../components/widgetSm/widgetSm';
 import './Home.css'
 
 export default function Home() {
@@ -9,6 +10,9 @@ export default function Home() {
     <div className="home">
       <Feature />
       <Chart grid title="Month Sale" data={xAxisData} dataKey="Sale" />
+      <div className="homeWidgets">
+        <WidgetSm />
+      </div>
     </div>
   )
 }
